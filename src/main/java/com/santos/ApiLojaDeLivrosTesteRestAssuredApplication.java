ba606ad64e8996b404039e45a67af3d864a8d4c6
leafar-Santos <leafarccomputacao@gmail.com>
2024-01-08 16:34:24 -0300

    Criação do projeto loja de livros

---
+++ src/main/java/com/santos/ApiLojaDeLivrosTesteRestAssuredApplication.java
@@ -0,0 +1,13 @@
+package com.santos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiLojaDeLivrosTesteRestAssuredApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiLojaDeLivrosTesteRestAssuredApplication.class, args);
+	}
+
+}
